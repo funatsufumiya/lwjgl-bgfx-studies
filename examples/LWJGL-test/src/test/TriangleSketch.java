d48f1da6dfeb26f68d5fdd43691adec079c3db69
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-01-03 11:38:19 +0900

    reverted TriangleSketch

--- examples/LWJGL-test/src/test/TriangleSketch.java
+++ examples/LWJGL-test/src/test/TriangleSketch.java
@@ -1,17 +1,10 @@
 package test;
 
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.FloatBuffer;
 
-import org.joml.Matrix4f;
-import org.joml.Matrix4x3f;
-import org.joml.Vector3f;
-import org.lwjgl.bgfx.BGFX;
 import static org.lwjgl.bgfx.BGFX.BGFX_CLEAR_COLOR;
 import static org.lwjgl.bgfx.BGFX.BGFX_CLEAR_DEPTH;
 import static org.lwjgl.bgfx.BGFX.BGFX_DEBUG_TEXT;
-import static org.lwjgl.bgfx.BGFX.BGFX_DEBUG_WIREFRAME;
 import static org.lwjgl.bgfx.BGFX.BGFX_STATE_DEFAULT;
 import static org.lwjgl.bgfx.BGFX.bgfx_dbg_text_clear;
 import static org.lwjgl.bgfx.BGFX.bgfx_dbg_text_printf;
@@ -26,9 +19,7 @@ import static org.lwjgl.bgfx.BGFX.bgfx_encoder_set_vertex_buffer;
 import static org.lwjgl.bgfx.BGFX.bgfx_encoder_submit;
 import static org.lwjgl.bgfx.BGFX.bgfx_set_debug;
 import static org.lwjgl.bgfx.BGFX.bgfx_set_view_clear;
-import static org.lwjgl.bgfx.BGFX.bgfx_set_view_transform;
 import org.lwjgl.bgfx.BGFXVertexLayout;
-import org.lwjgl.system.MemoryUtil;
 
 import sketch.App;
 import sketch.Sketch;
@@ -38,9 +29,6 @@ import static sketch.util.BGFXUtil.VertexLayoutType.XYC;
 import static sketch.util.BGFXUtil.byteSizeOf;
 
 public class TriangleSketch extends Sketch {
-    static int width = 640;
-    static int height = 480;
-
     private BGFXVertexLayout layout;
 
     private static final Object[][] kTriangleVertices = {
@@ -51,16 +39,6 @@ public class TriangleSketch extends Sketch {
 
     int kTriangleIndices[] = {0, 1, 2};
 
-    Matrix4f proj = new Matrix4f();
-    Matrix4x3f view = new Matrix4x3f();
-    Matrix4x3f model = new Matrix4x3f();
-    FloatBuffer proj_buffer;
-    FloatBuffer view_buffer;
-    FloatBuffer model_buffer;
-
-    ByteBuffer vertices;
-    ByteBuffer indices;
-
     short vertex_buffer = -1;
     short index_buffer = -1;
     short program = -1;
@@ -68,22 +46,13 @@ public class TriangleSketch extends Sketch {
     @Override
     public void setup() {
         // Enable debug text.
-        // bgfx_set_debug(BGFX_DEBUG_TEXT);
-        bgfx_set_debug(BGFX_DEBUG_TEXT | BGFX_DEBUG_WIREFRAME);
-        // bgfx_set_debug(BGFX_DEBUG_TEXT | BGFX_DEBUG_STATS);
+        bgfx_set_debug(BGFX_DEBUG_TEXT);
         bgfx_set_view_clear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0x303030ff, 1.0f, 0);
-
-        proj_buffer = MemoryUtil.memAllocFloat(16);
-        view_buffer = MemoryUtil.memAllocFloat(16);
-        model_buffer = MemoryUtil.memAllocFloat(16);
 
         layout = BGFXUtil.createVertexLayout2D(false, true, 0);
 
-        vertices = MemoryUtil.memAlloc(byteSizeOf(XYC, 3));
-        indices = MemoryUtil.memAlloc(kTriangleIndices.length * 2);
-
-        vertex_buffer = BGFXUtil.createVertexBuffer(vertices, layout, kTriangleVertices);
-        index_buffer = BGFXUtil.createIndexBuffer(indices, kTriangleIndices);
+        vertex_buffer = BGFXUtil.createVertexBuffer(byteSizeOf(XYC, 3), layout, kTriangleVertices);
+        index_buffer = BGFXUtil.createIndexBuffer(kTriangleIndices);
         try {
             program = BGFXUtil.createBasicShaderProgram();
         } catch (IOException ex) {
@@ -98,22 +67,9 @@ public class TriangleSketch extends Sketch {
         background(125);
         // background(255);
 
-        BGFXUtil.lookAt(new Vector3f(0.0f, 0.0f, 0.0f), new Vector3f(0.0f, 0.0f, -35.0f), view);
-        BGFXUtil.perspective(60.0f, width, height, 0.1f, 100.0f, proj);
-
-        bgfx_set_view_transform(0, view.get4x4(view_buffer), proj.get(proj_buffer));
-
         long encoder = bgfx_encoder_begin(false);
 
         // draw triangle
-
-        float t = elapsedTimeSeconds();
-        float s = (float) Math.sin(t) * 100.0f;
-
-        BGFX.bgfx_encoder_set_transform(encoder,
-            // model.identity().rotateY(elapsedTimeSeconds()).get4x4(model_buffer)
-            model.translation(s, s, 0.0f).rotateY(t).get4x4(model_buffer)
-        );
 
         bgfx_encoder_set_vertex_buffer(encoder, 0, vertex_buffer, 0, 3);
         bgfx_encoder_set_index_buffer(encoder, index_buffer, 0, 3);
@@ -138,20 +94,13 @@ public class TriangleSketch extends Sketch {
 
     @Override
     public void exit() {
-        MemoryUtil.memFree(proj_buffer);
-        MemoryUtil.memFree(view_buffer);
-        MemoryUtil.memFree(model_buffer);
-
         bgfx_destroy_program(program);
         bgfx_destroy_index_buffer(index_buffer);
-        MemoryUtil.memFree(indices);
         bgfx_destroy_vertex_buffer(vertex_buffer);
-        MemoryUtil.memFree(vertices);
-
         layout.free();
     }
 
     public static void main(String[] args) {
-        App.main("test.TriangleSketch", width, height);
+        App.main("test.TriangleSketch", 640, 480);
     }
 }
